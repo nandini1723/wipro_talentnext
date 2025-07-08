058d1fe67d7af8714e7064a41671fc5c6900e5b2
nandini1723 <mallineninandini@gmail.com>
2025-07-08 11:36:41 +0530

    javafundamentals

---
+++ javafundamentals/src/javafundamentals/Question2.java
@@ -0,0 +1,13 @@
+package javafundamentals;
+import java.util.*;
+public class Question2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		String name=sc.nextLine();
+		System.out.println("Welcome "+name);
+
+	}
+
+}
