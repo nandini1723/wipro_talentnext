54dfb0bc1ec2fa8acb5cb12402b53c30cf88f5a6
nandini1723 <mallineninandini@gmail.com>
2025-07-08 11:24:13 +0530

    Java_Fundamentals

---
+++ javafundamentals/src/javafundamentals/Question1.java
@@ -0,0 +1,14 @@
+package javafundamentals;
+import java.util.*;
+public class Question1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		String a=sc.nextLine();
+		String b=sc.nextLine();
+		System.out.println(a+ " technologies "+b);
+
+	}
+
+}
